e1c5921920357f38d4cc9d637ed9de2e849cd4cf
ddehaven <ddehaven@adobe.com>
2018-09-03 16:38:29 -0700

    added test text

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const layout = ( props ) => (
-	<>
+	<React.Fragment>
 		<div>Toolbar, SideDrawer,Backdrop</div>
 		<main>
 			{props.children}
 		</main>
-	</>
+	</React.Fragment>
 )
+
+export default layout;
